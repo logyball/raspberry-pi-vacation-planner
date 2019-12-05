0a33733dd4ca467b77dce4178f129154456d65c4
loganballard <33067676+loganballard@users.noreply.github.com>
2019-12-05 11:22:59 -0800

    todays weather forcast complete

--- resources/left_sidebar.py
+++ resources/left_sidebar.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import (
-    QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy
+    QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy, QLabel
 )
 from PyQt5.QtCore import Qt
+from helpers.weather import get_weather_info
 from helpers.config import get_width, get_height
 from helpers.testing_utils import set_random_background_color
 from resources.weather_widgets import CurrentTemp, CurrentIcon
@@ -12,17 +13,17 @@ class WeatherBar(QWidget):
     right_temp: CurrentTemp = None
     layout: QHBoxLayout = None
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, todays_weather={}):
         super(WeatherBar, self).__init__(parent)
-        self.initUI()
+        self.initUI(todays_weather=todays_weather)
 
-    def initUI(self):
+    def initUI(self, todays_weather):
         self.layout = QHBoxLayout()
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.left_icon = CurrentIcon(parent=self)
+        self.left_icon = CurrentIcon(parent=self, weather_icon_path=todays_weather.get("icon"))
         self.left_icon.setFixedWidth(int(get_width() * 0.15))
         self.left_icon.setIcon()
-        self.right_temp = CurrentTemp(parent=self)
+        self.right_temp = CurrentTemp(parent=self, cur_temp=todays_weather.get("temp"))
         self.right_temp.setFixedWidth(int(get_width() * 0.15))
         self.layout.addWidget(self.left_icon, alignment=Qt.AlignHCenter)
         self.layout.addWidget(self.right_temp, alignment=Qt.AlignHCenter)
@@ -31,26 +32,26 @@ class WeatherBar(QWidget):
 
 class LeftSidebar(QWidget):
     top_weather_bar: WeatherBar = None
-    middle_snow_report: QWidget = None
+    middle_snow_report: QLabel = None
     bottom_weather_forecast: QWidget = None
     layout: QVBoxLayout = None
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, resort="killington"):
         super(LeftSidebar, self).__init__(parent)
-        self.initUI()
+        weather_info = get_weather_info(resort)
+        self.initUI(weather_info=weather_info)
 
-    def initUI(self):
+    def initUI(self, weather_info):
         self.layout = QVBoxLayout()
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.top_weather_bar = WeatherBar(parent=self)
-        self.middle_snow_report = QWidget(parent=self)
+        self.top_weather_bar = WeatherBar(parent=self, todays_weather=weather_info.get('today'))
+        self.middle_snow_report = QLabel(weather_info.get("today").get("details"), parent=self)
+        self.middle_snow_report.setWordWrap(True)
         self.bottom_weather_forecast = QWidget(parent=self)
         self.top_weather_bar.setFixedHeight(int(get_height() * 0.25))
         self.middle_snow_report.setMinimumHeight(int(get_height() * 0.30))
         self.middle_snow_report.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.bottom_weather_forecast.setMinimumHeight(int(get_height() * 0.4))
-        for w in (self.middle_snow_report, self.bottom_weather_forecast):
-            set_random_background_color(w)
         self.layout.addWidget(self.top_weather_bar, alignment=Qt.AlignTop)
         self.layout.addWidget(self.middle_snow_report, alignment=Qt.AlignVCenter)
         self.layout.addWidget(self.bottom_weather_forecast, alignment=Qt.AlignBottom)
--- resources/weather_widgets.py
+++ resources/weather_widgets.py
@@ -4,30 +4,27 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt
 from helpers.config import get_width, get_height
-from helpers.weather import get_current_temp_resort, get_current_weather_icon_resort
-from helpers.testing_utils import set_random_background_color
+
 
 class CurrentIcon(QWidget):
     pic_map: QPixmap = None
     icon: QLabel = None
+    icon_path: str = None
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, weather_icon_path=''):
         super(CurrentIcon, self).__init__(parent)
+        self.icon_path = weather_icon_path
         self.initUI()
 
     def initUI(self):
         self.icon = QLabel(self)
-        set_random_background_color(self)
 
     def setIcon(self):
-        self.pic_map = QPixmap(
-            get_current_weather_icon_resort('killington')
-        ).scaled(
+        self.pic_map = QPixmap(self.icon_path).scaled(
             self.width(),
             self.width()
         )
         self.icon.setPixmap(self.pic_map)
-
 
 
 class CurrentTemp(QWidget):
@@ -35,17 +32,15 @@ class CurrentTemp(QWidget):
     temp: QLabel = None
     layout: QVBoxLayout = None
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, cur_temp=''):
         super(CurrentTemp, self).__init__(parent)
-        self.initUI()
+        self.initUI(cur_temp=cur_temp)
 
-    def initUI(self):
+    def initUI(self, cur_temp):
         self.layout = QVBoxLayout()
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.label = QLabel("Current Temp")
-        self.temp = QLabel(
-            str(get_current_temp_resort('killington'))
-        )
+        self.temp = QLabel(cur_temp)
         self.label.setFixedHeight(int(get_height() * 0.05))
         self.temp.setFixedHeight(int(get_height() * 0.2))
         self.temp.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
